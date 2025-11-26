2a8a6f5cfc93bc7c37f89a816147231cbee1de95
Blessing Tembo <temboblessing05@gmail.com>
2025-11-26 12:57:21 +0200

    replace links name with link

--- scripts/index.js
+++ scripts/index.js
@@ -12,43 +12,16 @@ if (searchButton) {
 }
 
 function saveSearchValue() {
-
   const animeName = inputElem.value;
   const encoded = encodeURIComponent(animeName);
-   window.location.href = `home.html?search=${encoded}`
+  window.location.href = `home.html?search=${encoded}`
 }
 
 // links
-
-const trandingLink = document.querySelector('.js-tranding-link');
-const recentLink = document.querySelector('.js-recent-link');
-const homeLink = document.querySelector('.js-home-link');
-const newLink = document.querySelector('.js-new-link');
-
-if (homeLink) {
-  homeLink.addEventListener('click', (event) => {
-    const category = event.target.dataset.category
-    window.location.href = `home.html?query=${category}`
+document.querySelectorAll('.js-link')
+  .forEach((link) => {
+    link.addEventListener('click', (event) => {
+      const category = event.target.dataset.category
+      window.location.href = `home.html?query=${category}`
+    })
   })
-}
-
-if (trandingLink) {
-  trandingLink.addEventListener('click', (event) => {
-    const category = event.target.dataset.category
-    window.location.href = `home.html?query=${category}`
-  })
-}
-
-if (newLink) {
-  newLink.addEventListener('click', (event) => {
-    const category = event.target.dataset.category
-    window.location.href = `home.html?query=${category}`
-  })
-}
-
-if (recentLink) {
-  recentLink.addEventListener('click', (event) => {
-    const category = event.target.dataset.category
-    window.location.href = `home.html?query=${category}`
-  })
-}
